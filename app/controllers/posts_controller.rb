520050a805e3418a28ce7be2fd7b7c3979288432
ThomasDebek <tom.debek@gmail.com>
2016-10-03 18:07:34 +0200

    Kontroller, formularz, widoki

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,5 +4,20 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "Dziekujemy za wpis"
+      redirect_to posts_path
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:author, :body)
+  end
+
+
 end
