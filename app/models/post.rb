280bb797a8ff5e35d431230fad471f6e1f08c9c5
ThomasDebek <tom.debek@gmail.com>
2016-10-04 18:36:05 +0200

    Scopes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ActiveRecord::Base
   validates :body, presence: true
+
+  before_create :titleize_author
+
+
+  private
+  def titleize_author
+    self.author = self.author.to_s.titleize
+  end
+
 end
