9fa9e5672392e8f021225c4b5f91b1ce22399517
ThomasDebek <tom.debek@gmail.com>
2016-10-05 20:09:49 +0200

    Asocjacja belongs_to i has_many

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,21 @@
 class Post < ActiveRecord::Base
-  validates :body, presence: true
 
-  before_create :titleize_author
+  has_many :comments
 
 
-  private
-  def titleize_author
-    self.author = self.author.to_s.titleize
-  end
+
+  #validates :body, presence: true
+  #before_create :titleize_author
+
+
+#  scope :published, -> { where(author: true )}
+ # scope :ordered, -> { order(created_at: :desc)}
+  #scope :created_before, -> { where("created_at < ?" ) }
+
+
+  #private
+ # def titleize_author
+  #  self.author = self.author.to_s.titleize
+ # end
 
 end
